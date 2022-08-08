47568dd2bb3d8341a65226a214858cc8a04a019c
Raunak Rathore <64827299+Error404R@users.noreply.github.com>
2022-08-08 17:06:35 +0530

    Create app.py

---
+++ Deep Learning Part/app.py
@@ -0,0 +1,66 @@
+from flask import Flask, render_template, request
+ 
+import numpy as np
+import os
+ 
+from keras.preprocessing.image import load_img
+from keras.preprocessing.image import img_to_array
+from keras.models import load_model
+ 
+#load model
+model =load_model("model/redixolabs.h5")
+ 
+print('@@ Model loaded')
+ 
+ 
+def pred_cot_dieas(cott_plant):
+  test_image = load_img(cott_plant, target_size = (150, 150)) 
+  print("@@ Got Image for prediction")
+   
+  test_image = img_to_array(test_image)/255 
+  test_image = np.expand_dims(test_image, axis = 0) 
+   
+  result = model.predict(test_image).round(3) 
+  print('@@ Raw result = ', result)
+   
+  pred = np.argmax(result) 
+ 
+  if pred == 0:
+    return "Healthy Plant", 'healthy.html' 
+  elif pred == 1:
+      return 'Diseased Plant', '1.html' 
+  elif pred == 2:
+      return 'Diseased Plant', '2.html'  
+  else:
+    return "Healthy Plant", 'healthy.html'
+ 
+ 
+ 
+#------------>>pred_cot_dieas<<--end
+     
+app = Flask(__name__)
+ 
+# render index.html page
+@app.route("/", methods=['GET', 'POST'])
+def home():
+        return render_template('index.html')
+     
+  
+# get input image from client then predict class and render respective .html page for solution
+@app.route("/predict", methods = ['GET','POST'])
+def predict():
+     if request.method == 'POST':
+        file = request.files['image'] 
+        filename = file.filename        
+        print("@@ Input posted = ", filename)
+         
+        file_path = os.path.join('static/user uploaded', filename)
+        file.save(file_path)
+ 
+        print("@@ Predicting class......")
+        pred, output_page = pred_cot_dieas(cott_plant=file_path)
+               
+        return render_template(output_page, pred_output = pred, user_image = file_path)
+     
+if __name__ == "__main__":
+    app.run(threaded=False) 
